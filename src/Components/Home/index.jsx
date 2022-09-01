6abc7051435daba056e547ae51bd7b675f1a3df8
viktorsuster <viktor.suster.mail@gmail.com>
2022-09-01 16:17:34 +0200

    Handling fetch errors

--- src/Components/Home/index.jsx
+++ src/Components/Home/index.jsx
@@ -4,6 +4,7 @@ import BlogList from '../BlogList'
 const Home = () => {
   const [blogs, setBlogs] = useState(null)
   const [isPending, setIsPending] = useState(true)
+  const [error, setError] = useState(null)
 
   const handleDelete = (id) => {
     const newBlogs = blogs.filter(blog => blog.id !== id)
@@ -14,16 +15,24 @@ const Home = () => {
     setTimeout(() => {
       fetch('http://localhost:8000/blogs')
      .then(res => {
+      if (res.ok) {
+        throw Error('Could not fetch the data for that resource')
+      }
       return res.json()
      })
      .then(data => {
       setBlogs(data)
       setIsPending(false)
      })
+     .catch((err) => {
+      setIsPending(false)
+      setError(err.message)
+     })
     }, 1000)
   }, [])
   return (
     <div className='home'>
+      {error && <div>{ error }</div>}
       {isPending && <div>Loading...</div>}
       {blogs && <BlogList blogs={blogs} title="Blogs" handleDelete={handleDelete} />}
       </div>
