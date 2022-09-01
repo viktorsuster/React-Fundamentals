4686224fe03b0074cd6fc4c12112c3929e818dc2
viktorsuster <viktor.suster.mail@gmail.com>
2022-09-01 15:23:53 +0200

    Functions as props

--- src/Components/Home/index.jsx
+++ src/Components/Home/index.jsx
@@ -7,9 +7,14 @@ const Home = () => {
     { title: "Welcome", body: "lorem ipsum...", author: "Peter", id: 2 },
     { title: "Ou yeah", body: "lorem ipsum...", author: "Jacob", id: 3 }
   ])
+
+  const handleDelete = (id) => {
+    const newBlogs = blogs.filter(blog => blog.id !== id)
+    setBlogs(newBlogs)
+  }
   return (
     <div className='home'>
-      <BlogList blogs={blogs} title="Blogs"/>
+      <BlogList blogs={blogs} title="Blogs" handleDelete={handleDelete} />
       <BlogList blogs={blogs.filter((blog) => blog.author === "Viktor")} title="Viktor" />
       <BlogList blogs={blogs.filter((blog) => blog.author === "Peter")} title="Peter" />
       <BlogList blogs={blogs.filter((blog) => blog.author === "Jacob")} title="Jacob" />
