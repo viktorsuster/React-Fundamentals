9dc3d95d11094af1e6ba241959d8c88a73e4a8b9
viktorsuster <viktor.suster.mail@gmail.com>
2022-09-01 16:32:48 +0200

    Making a custom hook

--- src/Components/Home/index.jsx
+++ src/Components/Home/index.jsx
@@ -1,40 +1,15 @@
 import React, {useState, useEffect} from 'react'
+import useFetch from '../../useFetch'
 import BlogList from '../BlogList'
 
 const Home = () => {
-  const [blogs, setBlogs] = useState(null)
-  const [isPending, setIsPending] = useState(true)
-  const [error, setError] = useState(null)
+  const { data: blogs, isPending, error } = useFetch('http://localhost:8000/blogs')
 
-  const handleDelete = (id) => {
-    const newBlogs = blogs.filter(blog => blog.id !== id)
-    setBlogs(newBlogs)
-  }
-
-  useEffect(() => {
-    setTimeout(() => {
-      fetch('http://localhost:8000/blogs')
-     .then(res => {
-      if (res.ok) {
-        throw Error('Could not fetch the data for that resource')
-      }
-      return res.json()
-     })
-     .then(data => {
-      setBlogs(data)
-      setIsPending(false)
-     })
-     .catch((err) => {
-      setIsPending(false)
-      setError(err.message)
-     })
-    }, 1000)
-  }, [])
   return (
     <div className='home'>
       {error && <div>{ error }</div>}
       {isPending && <div>Loading...</div>}
-      {blogs && <BlogList blogs={blogs} title="Blogs" handleDelete={handleDelete} />}
+      {blogs && <BlogList blogs={blogs} title="Blogs" />}
       </div>
   )
 }
