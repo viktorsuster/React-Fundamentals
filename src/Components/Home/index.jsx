92aed73b974b5a8949e205ae6b12b8785519a07f
viktorsuster <viktor.suster.mail@gmail.com>
2022-09-01 12:05:17 +0200

    Click event

--- src/Components/Home/index.jsx
+++ src/Components/Home/index.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
 
 const Home = () => {
+  const handleClick =  () => {
+    console.log("hello")
+  }
   return (
-    <div>Home</div>
+    <div className='home'>
+      <h1>Home</h1>
+      <button onClick={handleClick}>Click</button></div>
   )
 }
 
