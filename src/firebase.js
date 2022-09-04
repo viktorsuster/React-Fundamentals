4292147bb53d30592f5e7e54e1a4a3bf2b68b2cb
viktorsuster <viktor.suster.mail@gmail.com>
2022-09-04 15:06:04 +0200

    Initialize firebase from local enviroment variable

--- src/firebase.js
+++ src/firebase.js
@@ -3,14 +3,14 @@ import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDdyDkCEJ-kpD3S6mjY6yKmzz4OyRqinV0",
-  authDomain: "blog-4386b.firebaseapp.com",
-  databaseURL: "https://blog-4386b-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "blog-4386b",
-  storageBucket: "blog-4386b.appspot.com",
-  messagingSenderId: "539684986719",
-  appId: "1:539684986719:web:40a9c6250e33be9f4160ea",
-  measurementId: "G-4LPH7QWMP6"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
